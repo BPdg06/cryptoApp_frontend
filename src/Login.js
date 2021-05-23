18f40504e7655adb69317d0bd76f48cce4d8b92f
kirk <kirk.betzina@gmail.com>
2021-05-23 17:04:50 -0400

    initial commit on feature branch and made Login component.

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Login = (props) => {
+    <div className='login'>
+        <input className='username'>username</input>
+        <input className='passowrd'>password</input>
+        <button className='submit' onClick>Submit</button>
+    </div>
+}
+export default Login
