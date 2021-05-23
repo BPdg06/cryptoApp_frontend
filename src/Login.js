3911f6b5651cee2f2a7421c847df1a651271285e
kirk <kirk.betzina@gmail.com>
2021-05-23 17:38:33 -0400

    added a form to login

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Login = (props) => {
-    <div className='login'>
-        <input className='username'>username</input>
-        <input className='passowrd'>password</input>
-        <button className='submit' onClick>Submit</button>
-    </div>
+    
+    return (
+        <>
+            <Form inline>
+            <FormGroup>
+                <Label for="exampleEmail" hidden>email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+            </FormGroup>
+            {' '}
+            <FormGroup>
+                <Label for="examplePassword" hidden>Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+            </FormGroup>
+            {' '}
+            <Button>Submit</Button>
+            
+            </Form>
+            <a href=''>forgot username/password</a>
+        </>
+      );
 }
 export default Login
