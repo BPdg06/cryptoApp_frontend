8a7663cd012c648c179242dbc0bed04a9dab65e5
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-25 09:57:57 -0700

    added getApiData function

--- src/App.js
+++ src/App.js
@@ -51,7 +51,14 @@ function App() {
     })
   }
 
-
+  const getApiData = () => {
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    fetch(url)
+    .then((response) => (response.json()))
+    .then((data) => {
+      setCoins(data)
+    })
+  }
 
   ///////////////////////////////
   // Render
