d78d774fc147cd1ccf956150838b92a5c3ebe3a0
Taylor Hughes <taylorhughes@Taylors-MacBook-Air.local>
2021-05-27 08:33:49 -0700

    save before merge

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,6 +1,40 @@
 import React from "react"
 
 const Transactions = (props) => {
+        
+    //////////////////////////
+    // Constants
+    //////////////////////////
+    
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    //////////////////////////
+    // Functions
+    //////////////////////////
+    
+    let dateArray = []
+    const transDates = props.transactions.map((item, index) => {
+        const today = new Date()
+        const date = new Date(String(item.createdAt))
+        const month = monthNames[date.getMonth()]
+        const day = date.getDate()
+        const year = date.getFullYear()
+        const newDate = `${day} ${month}, ${year}`
+        dateArray.push(newDate)
+        return ({
+            date: newDate,
+            data: item,
+        })
+    })
+
+    dateArray = [... new Set(dateArray)]
+
+    console.log(transDates, dateArray)
+
+    //////////////////////////
+    // Render
+    //////////////////////////
+
     return (
         <h2>This is the Transactions page.</h2>
     )
